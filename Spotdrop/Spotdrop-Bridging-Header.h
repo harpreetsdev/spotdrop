001418a7fbd4fcad58958b81cd91bbbe8e1fc90d
Erik Koebke <erikkoebke@eriks-mbp.home>
2016-01-11 20:56:28 -0500

    Initial app creation. Facebook SDK and sign-up. AWS SDK integrated.

---
+++ Spotdrop/Spotdrop-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Spotdrop-Bridging-Header.h
+//  Spotdrop
+//
+//  Created by Erik Koebke on 1/9/16.
+//  Copyright © 2016 Erik Koebke. All rights reserved.
+//
+
+#ifndef Spotdrop_Bridging_Header_h
+#define Spotdrop_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Spotdrop_Bridging_Header_h */
--- Spotdrop/ViewController.swift
+++ Spotdrop/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
 
 
 }
